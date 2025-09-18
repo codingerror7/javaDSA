cc60c958a7406e6ac7ddb6350af84c1ad52b67a0
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2025-09-19 01:34:19 +0530

    dsa.java

--- dsa.java
+++ dsa.java
@@ -112,4 +112,30 @@ public class dsa{
 
 
 
+//PROGRAMME OF BINARY SEARCH ALGORITHM:
+import java.util.*;
 
+public class dsa{
+    public static int binarySearch(int numbers[],int key){
+        int start = 0 , end = numbers.length-1;
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(key==numbers[mid]){
+                return mid;
+            }
+         else if(key > numbers[mid]){  //right side:
+            start = mid + 1;
+         }
+         else if(key < numbers[mid]){  //left side:
+            end = mid-1;
+         }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int numbers[] = {10,20,30,40,50,60,70,80,90};  //it works only on sorted array in ascending order.
+        int key = 50;
+        int res = binarySearch(numbers,key);
+        System.out.println("KEY IS AT INDEX: " + res);
+    }
+}
