e59d68327401cab26e2d6f2dd4b2c31127d07cdf
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2025-10-11 17:34:37 +0530

    dsa.java

--- dsa.java
+++ dsa.java
@@ -537,3 +537,40 @@ INHERITANCE:
          p2.cutter();
      }
  }
+
+
+
+//BUBBLE SORT:
+import java.util.Scanner;
+
+public class dsa{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter elements:");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("array is:");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        //SORTING:
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    //SWAPPING:
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("sorted array is:");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
