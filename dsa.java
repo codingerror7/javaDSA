20b484a1e7dd8c231fdd1e2d53eeefea7d84dde7
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2025-10-23 19:46:46 +0530

    Update dsa.java

--- dsa.java
+++ dsa.java
@@ -599,3 +599,18 @@ public class dsa{
         printNum(1);
     }
 }
+
+
+public class dsa{
+    public static void printNum(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printNum(n-1);
+    }
+    public static void main(String args[]){
+        printNum(10);
+    }
+}
